e1eccfbed7f14e473896037cd8398da0d619c030
0706693 <0706693@rgu.ac.uk>
2019-03-04 11:16:18 +0000

    4/3/19

---
+++ superherodirectory/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_SERVER','CSDM-WEBDEV');
+define('DB_USERNAME','0706693');
+define('DB_PASSWORD','0706693');
+define('DB_DATABASE', 'db0706693_cmm007');
+
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
